052731df97885f79e3612ae16632f334d99d1588
Saleh Abdullahi <salehabdullahi018@gmail.com>
2026-06-21 14:02:04 +0100

    fixed error in controller

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -3,37 +3,43 @@ import bcrypt from 'bcrypt'
 import User from '../models/User.js'
 import Donor from '../models/Donor.js'
 
+export const loginUser = async (req, res) => {
+  try {
+    const { username, password } = req.body;
 
-export const loginUser = async(req,res,next)=>{
-    try{
-        const {username,password} = req.body
+    const user = await User.findOne({ username });
 
-        // checking if user exists
-        const user = await User.findOne({username})
-
-        if(!user){
-            return res.status(401).json({error:"Invalid username or password"})
-        }
-
-        // compare the password
-        const passwordCorrect = await bcrypt.compare(password,user.passwordHash)
-
-          if (!passwordCorrect) {
-      return res.status(401).json({
-        error: "Invalid username or password",
-      });
+    if (!user) {
+      return res.status(401).json({ error: "Invalid username or password" });
     }
 
-    // create token
-    const userForToken= {
-        username:user.username,
-        id: user.id
+    const passwordCorrect = await bcrypt.compare(
+      password,
+      user.passwordHash
+    );
 
+    if (!passwordCorrect) {
+      return res.status(401).json({ error: "Invalid username or password" });
     }
-    const token = jwt.sign(userForToken,process.env.SECRET,{expiresIn:"2d"})
+
+    const userForToken = {
+      username: user.username,
+      id: user._id,
+    };
+
+    const token = jwt.sign(userForToken, process.env.SECRET, {
+      expiresIn: "2d",
+    });
+
     const donor = await Donor.findOne({ user: user._id });
 
-    res.status(200).json({token,username:user.username, donor})
-
-    }catch(error){next()}
-}
+    return res.status(200).json({
+      token,
+      username: user.username,
+      donor,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
